77007cfbab9ac88b9639e8fc9e13e3f192276a6c
HP <HP@DESKTOP-I46N7EU>
2024-06-28 10:48:46 +0530

    Add project files.

---
+++ TestApi/Controllers/TestsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Task()
+        {
+            var task = new string[] { "Task1", "Task2", "Task3" };
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTask()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTask()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTask()
+        {
+            var somethingWentWrong = true;
+            if (somethingWentWrong)
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+    }
+}
